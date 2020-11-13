214cfe4d8c648124e60dbdebed0d4602c59d00cf
jeffsondag <59399431+jeffsondag@users.noreply.github.com>
2020-11-13 16:33:57 -0600

    Add files via upload

---
+++ Add_Management_to_AD_Group.ps1
@@ -0,0 +1,57 @@
+﻿$inputFile = "G:\NorthStar\HR_Export_Reports\ITOC\management_position_list.csv"
+$GroupName = "Orion-Company-Managers"
+
+$data = Import-CSV $inputFile
+$data | ForEach {
+    $management = $_.{This is a Management position}
+    $email = $_.{Work Contact: Work Email}
+    $GivenName = $_.{First Name}
+    $Surname = $_.{Last Name}
+    $systems = $false
+
+        if ($management -eq "Yes") {
+
+
+            $userObject = Get-ADUser -Filter "userPrincipalName -eq '$email'" 
+
+            if ($userObject -eq $null) {
+                $userObject = Get-ADUser -Filter "(GivenName -eq '$GivenName') -and (Surname -eq '$Surname')" 
+            }
+
+            if ($userObject -eq $null) {
+                $userObject = Get-ADUser -Filter "emailAddress -eq '$email'"
+            }
+
+            if ($userObject -eq $null) {
+                $userObject = Get-ADUser -Filter "proxyAddresses -like '*${email}*'"
+            }
+
+            if ($userObject -eq $null) {
+                $userObject = Get-ADUser -Filter "(GivenName -eq '$GivenName') -and (Surname -eq '$Surname')" -Server systems
+
+                if ($userObject -ne $null) {$systems = $True}
+            }
+
+            if ($userObject -eq $null) {
+                $userObject = $userObject = Get-ADUser -Filter "emailAddress -eq '$email'" -Server systems
+                
+                if ($userObject -ne $null) {$systems = $True}
+            }
+                
+
+            if ($userObject -eq $null) {
+                $userObject = Get-ADUser -Filter "proxyAddresses -like '*${email}*'" -Server systems
+                if ($userObject -ne $null) {$systems = $True}
+            }
+            
+            if ($userObject -ne $null) {Add-ADGroupMember -Identity $GroupName -Members $userObject}
+
+            
+            #$email
+            $userObject = $userObject.sAMAccountName
+            if ($systems) {write-host "systems\${userObject}"} else {
+            Write-Host $userObject
+            }
+            
+        }
+}
